8cd8725b6e322945663038b0d9235006ddc87719
timofei345 <120594945+timofei345@users.noreply.github.com>
2023-05-25 22:29:58 +0200

    Add files via upload

--- scripthw2.js
+++ scripthw2.js
@@ -1,23 +1,14 @@
 let date = new Date();
 
-let time = [ date.getDate(), date.getMonth(), date.getFullYear()];
+let answer1 = prompt("Введите день:");
+let answer2 = prompt("Введите месяц:");
 
-let months = date.getMonth();
-let xmonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let time = [date.getDate(), date.getMonth()];
 
-if( time[0] < 10 ){
-    time[0] = "0" + time[0];
-};
+if (time[0] < 10) {
+  time[0] = "0" + time[0];
+}
 
-let today = [time[0], xmonths[months], time[2]].join(".");
-
-console.log("Date: ",today)
-
-
-let answer = prompt("Введите дату рождения(Example: 01.January.2001):");
-
-if (answer === today){
-
-    alert("С днем рождения!")
-};
-
+if (answer1 === String(time[0]) && answer2 === String(time[1] + 1)) {
+  alert("С днем рождения!");
+}
